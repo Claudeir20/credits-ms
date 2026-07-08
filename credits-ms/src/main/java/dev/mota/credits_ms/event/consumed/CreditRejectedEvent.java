82d9c39f193ccc0b97ec6634d7735fc794ba58d9
Claudeir20 <claudeirmota2@gmail.com>
2026-07-08 01:29:42 -0300

    feat: add CreditRejectedEvent

--- credits-ms/src/main/java/dev/mota/credits_ms/event/consumed/CreditRejectedEvent.java
+++ credits-ms/src/main/java/dev/mota/credits_ms/event/consumed/CreditRejectedEvent.java
@@ -3,7 +3,7 @@ package dev.mota.credits_ms.event.consumed;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record CreditRejectEvent(
+public record CreditRejectedEvent(
         UUID eventId,
         UUID requestId,
         LocalDateTime occurred
